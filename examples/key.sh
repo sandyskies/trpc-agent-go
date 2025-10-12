4de7b1515c1a7f8b5bb7a108e2079485eefe8ad9
sandyskies <chenmingjie0828@163.com>
2025-10-12 22:14:34 +0800

    xx

---
+++ examples/key.sh
@@ -0,0 +1,9 @@
+export OPENAI_BASE_URL="https://api.deepseek.com/v1"
+export OPENAI_API_KEY="sk-2fb3486ea7224206a6b7d5e51ff7d189"
+export MODEL_NAME="deepseek-chat"
+
+export OPENAI_BASE_URL=http://wujipt.woa.com/api/v1
+export OPENAI_API_KEY=2fe60304d5192f22
+
+export OPENAI_BASE_URL=http://v2.open.venus.oa.com/llmproxy
+export OPENAI_API_KEY="e61mQl2cmuFl4uSmpQzQMjtG@3265"
